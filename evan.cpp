6737f3a43b892e58350498745c6c0f3543dfb63d
PannDev <86787046+PannDev@users.noreply.github.com>
2023-09-22 04:34:27 +0700

    add evan for daily use

---
+++ evan.cpp
@@ -0,0 +1,15 @@
+//? Header Files and namespaces
+#include<bits/stdc++.h>
+using namespace std;
+
+//? Code Shortners
+#define endl '\n'
+#define PannDev ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+//! main
+int main () { PannDev
+
+    
+
+    return 0;
+}
