c125f5fe54abfd7dbba8ab3837139ce0ed402531
PannDev <86787046+PannDev@users.noreply.github.com>
2023-10-13 18:55:49 +0700

    sttring problem

---
+++ 3_Array/1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+//! main
+int main () { 
+
+    // int box[4];
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout << "Masukan box ke-" << i << " : ";
+    //     cin >> box[i];
+    // }
+
+    // cout << "Hasilnya adalah...\n";
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout << "isi box ke-" << i << " : ";
+    //     cout << box[i] << endl;
+    // }
+
+   char kota[5] ={'y'};
+
+//    cout << "masukan nama kota : " << endl;
+//    cin >> kota;
+
+   kota[5] = {'m'};
+    cout << kota << endl; 
+
+
+
+    return 0;
+}
